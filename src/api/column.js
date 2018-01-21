8e11269ea61dce985e26bb901fa589393be1a827
@Morelearn <morelearn1990@gmail.com>
2018-01-21 10:51:45 +0800

    column edit

--- src/api/column.js
+++ src/api/column.js
@@ -5,144 +5,142 @@ export default [{
             'columnId|+1': 0,
             'columnName': '@cword(8,16)',
             'total': '@natural(0,20)',
-            'children|5':[{
+            'children|5': [{
                 'columnId|+1': 0,
                 'columnName': '@cword(8,16)',
                 'total': '@natural(0,20)',
-                'children|5':[{
+                'children|5': [{
                     'columnId|+1': 0,
                     'columnName': '@cword(8,16)',
                     'total': '@natural(0,20)',
-                    'children':[]
+                    'children': []
                 }]
             }]
         }]
     }
-},{
+}, {
     path: '/column/delete',
-    method:'DELETE',
+    method: 'DELETE',
     data: {
         'result': true
     }
-},{
+}, {
     path: '/column/edit',
-    method:'GET',
+    method: 'GET',
     data: {
-        'column': [
-            {
-                'cname':'Id',
-                'name':'id',
-                'value|+1':0,
-                'type':'auto'
-            },{
-                'cname':'名称',
-                'name':'name',
-                'value':'@cname(4,5)',
-                'type':'text'
-            },{
-                'cname':'内容模型',
-                'name':'articlemodel',
-                'value|1':[1,2,3],
-                'type':'select',
-                'source':[{
-                    'name':'普通文章',
-                    'value':1
-                },{
-                    'name':'图片集',
-                    'value':2
-                },{
-                    'name':'视频集',
-                    'value':3
+        'column': [{
+                'cname': 'Id',
+                'name': 'id',
+                'value|+1': 0,
+                'type': 'auto'
+            }, {
+                'cname': '名称',
+                'name': 'name',
+                'value': '@cname(4,5)',
+                'type': 'text'
+            }, {
+                'cname': '内容模型',
+                'name': 'articlemodel',
+                'value|1': [1, 2, 3],
+                'type': 'select',
+                'source': [{
+                    'name': '普通文章',
+                    'value': 1
+                }, {
+                    'name': '图片集',
+                    'value': 2
+                }, {
+                    'name': '视频集',
+                    'value': 3
                 }]
-            },{
-                'cname':'SEO标题',
-                'name':'seotitle',
-                'value':'@ctitle()',
-                'type':'text'
-            },{
-                'cname':'SEO关键词',
-                'name':'publicDate',
-                'value':'@cword()',
-                'type':'auto'
+            }, {
+                'cname': 'SEO标题',
+                'name': 'seotitle',
+                'value': '@ctitle()',
+                'type': 'text'
+            }, {
+                'cname': 'SEO关键词',
+                'name': 'publicDate',
+                'value': '@cword()',
+                'type': 'text'
             },
             {
-                'cname':'栏目描述',
-                'name':'description',
-                'value':'@cparagraph()',
-                'type':'textarea'
-            },{
-                'cname':'缩略图',
-                'name':'thumbnail',
-                'value|1':[{
-                    'url':'@dataImage("336x280")'
+                'cname': '栏目描述',
+                'name': 'description',
+                'value': '@cparagraph()',
+                'type': 'textarea'
+            }, {
+                'cname': '缩略图',
+                'name': 'thumbnail',
+                'value': [{
+                    'url': '@dataImage("336x280")'
                 }],
-                'type':'fileimg'
+                'type': 'fileimg'
             },
             {
-                'cname':'内容',
-                'name':'content',
-                'value':'@cparagraph(50)',
-                'type':'richtext'
+                'cname': '内容',
+                'name': 'content',
+                'value': '@cparagraph(50)',
+                'type': 'richtext'
             }
         ]
     }
-},{
+}, {
     path: '/column/new',
-    method:'GET',
+    method: 'GET',
     data: {
-        'column': [
-            {
-                'cname':'Id',
-                'name':'id',
-                'value':'',
-                'type':'auto'
-            },{
-                'cname':'名称',
-                'name':'name',
-                'value':'',
-                'type':'text'
-            },{
-                'cname':'内容模型',
-                'name':'articlemodel',
-                'value':'',
-                'type':'select',
-                'source':[{
-                    'name':'普通文章',
-                    'value':'1'
-                },{
-                    'name':'图片集',
-                    'value':'2'
-                },{
-                    'name':'视频集',
-                    'value':'3'
+        'column': [{
+                'cname': 'Id',
+                'name': 'id',
+                'value': '',
+                'type': 'auto'
+            }, {
+                'cname': '名称',
+                'name': 'name',
+                'value': '',
+                'type': 'text'
+            }, {
+                'cname': '内容模型',
+                'name': 'articlemodel',
+                'value': '',
+                'type': 'select',
+                'source': [{
+                    'name': '普通文章',
+                    'value': '1'
+                }, {
+                    'name': '图片集',
+                    'value': '2'
+                }, {
+                    'name': '视频集',
+                    'value': '3'
                 }]
-            },{
-                'cname':'SEO标题',
-                'name':'seotitle',
-                'value':'',
-                'type':'text'
-            },{
-                'cname':'SEO关键词',
-                'name':'publicDate',
-                'value':'',
-                'type':'auto'
+            }, {
+                'cname': 'SEO标题',
+                'name': 'seotitle',
+                'value': '',
+                'type': 'text'
+            }, {
+                'cname': 'SEO关键词',
+                'name': 'publicDate',
+                'value': '',
+                'type': 'text'
             },
             {
-                'cname':'栏目描述',
-                'name':'description',
-                'value':'',
-                'type':'textarea'
-            },{
-                'cname':'缩略图',
-                'name':'thumbnail',
-                'value':[],
-                'type':'fileimg'
+                'cname': '栏目描述',
+                'name': 'description',
+                'value': '',
+                'type': 'textarea'
+            }, {
+                'cname': '缩略图',
+                'name': 'thumbnail',
+                'value': [],
+                'type': 'fileimg'
             },
             {
-                'cname':'内容',
-                'name':'content',
-                'value':'',
-                'type':'richtext'
+                'cname': '内容',
+                'name': 'content',
+                'value': '',
+                'type': 'richtext'
             }
         ]
     }
--- src/pages/column/edit.vue
+++ src/pages/column/edit.vue
@@ -1,78 +1,157 @@
 <template>
-<div class="column-edit">
-
-    <el-dialog title="选择栏目模型" :visible.sync="columnModelSelect">
-        <el-form>
-            <el-form-item label="栏目模型">
-                <el-select v-model="columnModel" placeholder="请选择栏目模型">
-                    <el-option v-for="item in columnModelList" :key='item.id' label="item.name" value="item.id"></el-option>
+    <div class="column-edit">
+        <el-form label-position='right' size='small' label-width="100px" class="column-form">
+            <el-form-item v-for="(item,index) in column" :key='index' :label="item.cname" v-if='item.type != "auto"'>
+                <el-input v-if='item.type == "text"' :name='item.name' :value='item.value' class="input"></el-input>
+                <el-input v-else-if='item.type == "textarea"' type="textarea" :name='item.name' :value='item.value' :rows='4' class="input"></el-input>
+                <el-switch v-else-if='item.type == "switch"' v-model="item.value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+                <el-select v-else-if='item.type == "select"' v-model="item.value" placeholder="请选择">
+                    <el-option v-for="subitem in item.source" :key="subitem.value" :label="subitem.name" :value="subitem.value">{{subitem.name}}</el-option>
                 </el-select>
+                <el-upload v-else-if='item.type == "fileimg"' class="upload-demo" action="/article/edit/img" :multiple='false' :limit='1' :file-list="item.value" list-type="picture-card">
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
+                <div v-else-if='item.type == "richtext"' class="richtext">
+                    <quill-editor ref="myTextEditor" :options='richtextoption' v-model="item.value"></quill-editor>
+                </div>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="success" @click="save">保存</el-button>
+                <el-button @click="cancel">取消</el-button>
             </el-form-item>
         </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button type="primary" @click="save">确 定</el-button>
-        </div>
-    </el-dialog>
-</div>
+        <el-dialog title="选择栏目模型" :visible.sync="columnModelSelect">
+            <el-form>
+                <el-form-item label="栏目模型">
+                    <el-select v-model="columnModel" placeholder="请选择栏目模型">
+                        <el-option v-for="item in columnModelList" :key='item.id' :label="item.name" :value="item.id"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="selectModel">确 定</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 <style lang="less">
-
+.column-edit {
+  height: 100%;
+  width: 100%;
+  .column-form {
+    height: 100%;
+    padding: 20px;
+    overflow-y: scroll;
+    .input {
+      width: 50%;
+      min-width: 300px;
+    }
+  }
+  @import url("/node_modules/quill/dist/quill.core.css");
+  @import url("/node_modules/quill/dist/quill.snow.css");
+  @import url("/node_modules/quill/dist/quill.bubble.css");
+  .richtext {
+    .ql-picker-label > svg {
+      position: absolute;
+    }
+    .ql-picker-label::before {
+      position: absolute;
+    }
+    .ql-editor {
+      height: 450px;
+    }
+  }
+}
 </style>
 <script>
-    export default {
-        data: function () {
-            return {
-                parentColumnId: null,
-                type: 'new',
-                columnModel:'',
-                columnModelList:[],
-                columnModelSelect:false,
-                column:[],
-                err:''
-            }
-        },
-        created: function () {
-            var _this = this;
-            _this.columnId = _this.$route.params.columnId;
-            console.log(_this.$route.params.type)
-            _this.$route.params.type != 'new' ? console.log('aa'): _this.axios.get('/model/list/column').then((res) => {
-                    _this.columnModelList = res.data.columnModelList;
-                    _this.columnModelSelect = true;
-                });
-        },
-        methods:{
-            getColumnData(){
-                var _this = this;
-                var columnId = _this.columnId,
-                    type = _this.type,
-                    columnModel=_this.columnModel;
-                type !='new'?(() =>{
-                    _this.axios.get('/column/edit',{
-                        params: {
-                            columnId
-                        }
-                    }).then((res) =>{
-                        console.log(res)
-                        _this.column = res.data.column
-                    }).catch((err) =>{
-                        _this.err = err.toString();
-                    })
-                }):(() =>{
-                    _this.axios.get('/column/new',{
-                        params: {
-                            columnModel
-                        }
-                    }).then((res) =>{
-                        _this.column = res.data.column
-                    }).catch((err) =>{
-                        _this.err = err.toString();
-                    })
-                })
-            },
-            save(){
-                _this.columnModelSelect = false;
-                _this.getColumnData()
-            }
-        }
+import { quillEditor } from "vue-quill-editor";
+export default {
+  data: function() {
+    return {
+      parentColumnId: null,
+      type: "new",
+      columnModel: "",
+      columnModelList: [],
+      columnModelSelect: false,
+      column: [],
+      err: "",
+      richtextoption: {
+        theme: "snow"
+      }
+    };
+  },
+  components: {
+    quillEditor
+  },
+  created: function() {
+    var _this = this;
+    _this.columnId = _this.$route.params.columnId;
+    _this.$route.params.type != "new"
+      ? ((_this.type = "edit"), _this.getColumnData())
+      : _this.axios.get("/model/list/column").then(res => {
+          _this.columnModelList = res.data.lists;
+          _this.columnModelSelect = true;
+        });
+  },
+  methods: {
+    getColumnData() {
+      var _this = this;
+      var columnId = _this.columnId,
+        type = _this.type,
+        columnModel = _this.columnModel;
+      type != "new"
+        ? _this.axios
+            // 这儿需要增加查询参数
+            .get("/column/edit")
+            .then(res => {
+              _this.column = res.data.column;
+            })
+            .catch(err => {
+              _this.err = err.toString();
+            })
+        : _this.axios
+            // 这儿需要增加查询参数
+            .get("/column/new")
+            .then(res => {
+              _this.column = res.data.column;
+            })
+            .catch(err => {
+              _this.err = err.toString();
+            });
+    },
+    selectModel() {
+      this.columnModelSelect = false;
+      this.getColumnData();
+    },
+    cancel() {
+      this.$router.go(-1);
+    },
+    save() {
+      var _this = this;
+      _this.axios
+        .post("/article/edit/post", {
+          data: _this.article
+        })
+        .then(res => {
+          if (res.data.result) {
+            _this.$message({
+              type: "success",
+              message: "新增/修改成功"
+            });
+            _this.$router.go(-1);
+          } else {
+            throw "新增/修改失败";
+          }
+        })
+        .catch(err => {
+          _this.err = err.toString();
+          _this.$message({
+            type: "error",
+            message: "新增/修改失败"
+          });
+        });
     }
+  }
+};
 </script>
