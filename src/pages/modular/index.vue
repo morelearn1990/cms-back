635c7cb0585402819c78b525c8cdb893d7b8bb8a
@Morelearn <morelearn1990@gmail.com>
2018-01-21 17:03:59 +0800

    modular

--- src/pages/modular/index.vue
+++ src/pages/modular/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="modular"></div>
+    <div class="modular">
+        后期开发
+    </div>
 </template>
 <style lang="less" scoped>
-
+.modular {
+  padding: 20px;
+}
 </style>
 <script>
 export default {};
