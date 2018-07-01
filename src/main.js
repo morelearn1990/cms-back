71a7c6d84bd1a626430e69380d8bbb7c84dd4795
@Morelearn <morelearn1990@gmail.com>
2018-07-01 17:46:12 +0800

    更新，准备使用 graphQL

--- src/main.js
+++ src/main.js
@@ -21,11 +21,11 @@ import '../node_modules/quill/dist/quill.bubble.css'
 import App from './App.vue'
 import Routes from './router'
 // import Store from './store'
-// import Mock from './api/mockapi.js'
+// import Mock from './api/mock'
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
-    // Vue.use(Vuex)
+// Vue.use(Vuex)
 Vue.use(VueAxios, Axios);
 // Vue.use(Cookie)
 Vue.use(axios)
@@ -36,36 +36,38 @@ Vue.axios.defaults.headers.put['Content-Type'] = 'application/json';
 Vue.axios.defaults.headers.delete['Content-Type'] = 'application/json';
 
 const router = new VueRouter({
-    mode: 'history',
-    base: '/admin/',
-    routes: Routes
+  mode: 'history',
+  base: '/admin/',
+  routes: Routes
 });
 
 router.beforeEach((to, from, next) => {
-    // 改变网页 title
-    document.title = to.meta.title + ' CMS系统管理后台'
-    next()
+  // 改变网页 title
+  document.title = to.meta.title + ' CMS系统管理后台'
+  next()
 })
 
 // const store = new Vuex.Store(Store);
 
 new Vue({
-    el: '#m_cms_app',
-    router: router,
-    // store: store,
-    components: { App },
-    template: '<App/>',
-    watch: {
-        // "$route": 'checkLogined'
-    },
-    created() {
-        // this.checkLogined()
-    },
-    methods: {
-        // checkLogined() {
-        //     if (!this.getCookie('session') && this.$route.path !== '/login') {
-        //         this.$router.push('/login');
-        //     }
-        // }
-    }
+  el: '#m_cms_app',
+  router: router,
+  // store: store,
+  components: {
+    App
+  },
+  template: '<App/>',
+  watch: {
+    // "$route": 'checkLogined'
+  },
+  created() {
+    // this.checkLogined()
+  },
+  methods: {
+    // checkLogined() {
+    //     if (!this.getCookie('session') && this.$route.path !== '/login') {
+    //         this.$router.push('/login');
+    //     }
+    // }
+  }
 });
